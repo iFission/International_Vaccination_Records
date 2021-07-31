0ed12505f066f880b4c140e5b61f754d792ca579
iFission <alexwwj@icloud.com>
2021-07-31 20:56:39 +0800

    feat: add data structure for Record

--- backend/Records.sol
+++ backend/Records.sol
@@ -2,15 +2,22 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract Records {
 
+    struct Record{
+        string passportNumber;
+        string vaccineManufacturer;
+        string country;
+        uint timestamp;
+    }
+
     string[] passportNumbers;
-    mapping (string => string) records;
+    mapping (string => Record) records;
 
     function store(string memory passportNumber, string memory info) public {
         passportNumbers.push(passportNumber);
-        records[passportNumber] = info;
+        records[passportNumber] = Record({passportNumber: passportNumber, vaccineManufacturer: info, country: "Singapore", timestamp: block.timestamp});
     }
 
-    function retrieve(string memory passportNumber) public view returns (string memory){
-        return records[passportNumber];
+    function retrieve(string memory passportNumber) public view returns (string memory, string memory, string memory, uint){
+        return (records[passportNumber].passportNumber, records[passportNumber].vaccineManufacturer, records[passportNumber].country, records[passportNumber].timestamp);
     }
 }
