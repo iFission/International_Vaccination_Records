3e313a17228b53b14717d069f01b13c98ad1bb56
Jose Johnson <jose.johnson.jose@gmail.com>
2021-08-07 15:59:09 +0800

    chore: moved vaccinated above CountUp

--- frontend-analytics/src/components/CountryStats.js
+++ frontend-analytics/src/components/CountryStats.js
@@ -4,7 +4,6 @@ import CountUp from "react-countup";
 import "../css/CountryStats.css";
 
 const CountryStats = ({ filteredCountry }) => {
-  console.log(typeof(filteredCountry.vaccinated));
   return (
     <div>
             <div className="Stats">
@@ -14,26 +13,26 @@ const CountryStats = ({ filteredCountry }) => {
                 <p>{filteredCountry.countryName}</p>
                 {/* <p id="date">{new Date(country.date).toDateString()}</p> */}
                 <p>{filteredCountry.population}</p>
-                {/* <h2 id="main_result">
+                <h2 id="main_result">
                   <CountUp
                     end={Number(filteredCountry.population)}
                     // end={Number("111111111")}
                     duration={1}
                   />
-                </h2> */}
+                </h2>
               </div>
               <div className="vaccines">
                 <h3>Total Vaccinations</h3>
                 <div className="divider__line"></div>
                 <p>{filteredCountry.countryName}</p>
-                {/* <h2 id="main_result">
+                  <p>{filteredCountry.vaccinated}</p>
+                <h2 id="main_result">
                   <CountUp
                     end={Number(filteredCountry.vaccinated)}
                     // end={Number("111111111")}
                     duration={1}
                   />
-                </h2> */}
-                <p>{filteredCountry.vaccinated}</p>
+                </h2>
                 {/* <p id="date">{new Date(country.date).toDateString()}</p> */}
                 {/* <h2 id="vaccine_main_result">{filteredCountry.countryName}</h2> */}
               </div>
